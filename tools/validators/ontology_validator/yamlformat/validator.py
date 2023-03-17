80b1b9de11975743f99933a665e805bb1ba77e29
Jessy Ma <jessyma@google.com>
2023-03-17 18:26:47 -0500

    Update validator.py

--- tools/validators/ontology_validator/yamlformat/validator.py
+++ tools/validators/ontology_validator/yamlformat/validator.py
@@ -47,7 +47,7 @@ def main(parsed_args: argparse.ArgumentParser):
       path.expanduser(args.original),
       modified_types_filepath,
       interactive=ast.literal_eval(parsed_args.interactive),
-      parsed_args.skip_type_guid_checks)
+      not parsed_args.skip_type_guid_checks)
 
 if __name__ == '__main__':
   args = CreateParser().parse_args(sys.argv[1:])
